9a898ad84546ce9ed27480ad30fe03a660c85129
Hosung <an3735297@pusan.ac.kr>
2023-07-24 09:30:56 +0900

    옵저버 패턴 / 세팅 값 직접 전달 / 기상 스테이션 테스트

---
+++ design_pattern/src/main/java/me/design_pattern/observerPattern/WeatherStation.java
@@ -0,0 +1,12 @@
+package me.design_pattern.observerPattern;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentConditionsDisplay =
+                new CurrentConditionsDisplay(weatherData);
+
+        weatherData.setMeasurements(60, 40, 20.4f);
+    }
+}
